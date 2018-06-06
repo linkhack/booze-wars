9c85ad0ac97ab175b0c929cf6a38744c08405fd8
patricklink314 <patricklink314@gmail.com>
2018-06-06 20:47:11 +0200

    Revert "use shader in mesh"
    This reverts commit 2b52136d66bf108d1bb3a65271a05c24b8b1ac49.

--- BoozeWars/src/Mesh.cpp
+++ BoozeWars/src/Mesh.cpp
@@ -51,7 +51,6 @@ void Mesh::setupMesh()
 
 void Mesh::draw(Shader& shader)
 {
-	shader.use();
 	GLuint diffuseNr = 1;
 	GLuint specularNr = 1;
 	GLint backgroundlocation = glGetUniformLocation(shader.getID(), "colorTexture");
