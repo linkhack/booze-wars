753e6601f58851503b5251e16dfe4c8a71ad0012
patricklink314 <patricklink314@gmail.com>
2018-06-25 16:50:20 +0200

    stupid error fix

--- BoozeWars/src/Frustum.cpp
+++ BoozeWars/src/Frustum.cpp
@@ -12,12 +12,12 @@ Frustum::Frustum(myCamera& camera)
 	glm::vec4 row2 = glm::row(viewProj, 0);
 	glm::vec4 row3 = glm::row(viewProj, 0);
 	glm::vec4 row4 = glm::row(viewProj, 0);
-	planes[1] = row4 + row1;
-	planes[2] = row4 - row1;
-	planes[3] = row4 + row2;
-	planes[4] = row4 - row2;
-	planes[5] = row4 + row3;
-	planes[6] = row4 - row3;
+	planes[0] = row4 + row1;
+	planes[1] = row4 - row1;
+	planes[2] = row4 + row2;
+	planes[3] = row4 - row2;
+	planes[4] = row4 + row3;
+	planes[5] = row4 - row3;
 	cullingActivated = true;
 }
 
@@ -33,12 +33,12 @@ void Frustum::updatePlanes(myCamera& camera)
 	glm::vec4 row2 = glm::row(viewProj, 1);
 	glm::vec4 row3 = glm::row(viewProj, 2);
 	glm::vec4 row4 = glm::row(viewProj, 3);
-	planes[1] = row4 + row1;
-	planes[2] = row4 - row1;
-	planes[3] = row4 + row2;
-	planes[4] = row4 - row2;
-	planes[5] = row3;
-	planes[6] = row4 - row3;
+	planes[0] = row4 + row1;
+	planes[1] = row4 - row1;
+	planes[2] = row4 + row2;
+	planes[3] = row4 - row2;
+	planes[4] = row3;
+	planes[5] = row4 - row3;
 }
 
 bool Frustum::sphereInFrustum(glm::vec4 centerRadius)
@@ -46,7 +46,7 @@ bool Frustum::sphereInFrustum(glm::vec4 centerRadius)
 	float d;
 	float radius = centerRadius.w;
 	glm::vec3 center = glm::vec3(centerRadius.x, centerRadius.y, centerRadius.z);
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 6; ++i) {
 		d = distanceToPoint(planes[i], center);
 		if (d <= -radius) {
 			return cullingActivated || false;
